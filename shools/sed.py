0534e6a13a9ca69a719c814b5d86831d552d0c6d
samedamci <samedamci@disroot.org>
2021-08-11 19:55:44 +0200

    Fix bugs

--- shools/sed.py
+++ shools/sed.py
@@ -9,9 +9,12 @@ class SedException(Exception):
 
 def sed(input_str: str, expression: str) -> str:
     cmd = run(
-        ["sed", expression], input=bytes(input_str, encoding="utf-8"), stderr=PIPE
+        ["sed", expression],
+        input=bytes(input_str, encoding="utf-8"),
+        stdout=PIPE,
+        stderr=PIPE,
     )
     if cmd.returncode == 0:
-        return cmd.stdout
+        return str(cmd.stdout, encoding="utf-8")
     else:
         raise SedException(cmd.stderr)
